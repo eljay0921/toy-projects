16ed841c006556160e6c5fb5acd6cc74bd7ea432
dev.jin <lj7812@gmail.com>
2025-10-01 19:18:45 +0900

    coupon-rush-redis: 프로젝트 시작 (re)

---
+++ coupon-rush-redis/src/test/java/com/coupon/rush/CouponRushRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coupon.rush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponRushRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
